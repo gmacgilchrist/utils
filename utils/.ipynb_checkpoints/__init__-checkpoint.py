5214c3e4e691c5ba8e51aab5267cb3873b1c8f35
Graeme MacGilchrist <gmacgilchrist@gmail.com>
2021-05-24 18:12:12 -0700

    further functions

---
+++ utils/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,1 @@
+import utils.operations as operations
---
+++ utils/manipulation.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def midpoints_fromarray(array):
+    '''For a set of pandas interval objects in a numpy array,
+    return the midpoint.'''
